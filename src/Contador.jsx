359874715a99fcdc7df9afe9d07f7852dae124eb
Natanael Queiroz das Neves <natanaelqueiroz@gmail.com>
2022-07-29 00:34:19 -0300

    Botão Zerar

--- src/Contador.jsx
+++ src/Contador.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+<link rel="stylesheet" href="/dmtran-contador/public/style.css"></link>
 export default class Contador extends Component {
 
     constructor() {
@@ -29,6 +29,13 @@ export default class Contador extends Component {
         this.setState({carreta: this.state.carreta + 1})
     }
 
+    setZero() {
+        this.setState({carro: 0})
+        this.setState({moto: 0})
+        this.setState({onibus: 0})
+        this.setState({carreta: 0})
+    }
+
     render () {
         const {carro, moto, onibus, carreta} = this.state
         return (
@@ -42,7 +49,7 @@ export default class Contador extends Component {
                 <button id="moto" onClick={() => this.setMoto()}>2 = MOTO</button>
                 <button id="onibus" onClick={() => this.setOnibus()}>3 = ONIBUS E CAMINHÃO</button>
                 <button id="carreta" onClick={() => this.setCarreta()}>4 = CARRETA</button>
-            
+                <button id="zerar" onClick={() => this.setZero()}>Z = ZERAR</button>
             
             </div>
         )
